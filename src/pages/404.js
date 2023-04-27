dbad1f91ec6e65fca47dd72a2fdd962d3e7d0fe4
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-04-27 13:44:35 -0500

    changes to 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,7 @@
 import React from "react"
-import Layout from "../components/Layout"
-import Seo from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
-
-export const Head = () => <Seo title="404: Not Found" />
+const NotFoundPage = () => {
+  return <div>Here it is</div>
+}
 
 export default NotFoundPage
