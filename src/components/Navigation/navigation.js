f7e7248d4aabc0061209db1c1f9d6b983ca0a6f3
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-04-26 23:24:48 -0500

    setting up nav

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,10 +1,42 @@
 import React from "react"
 import Styles from "./navigation.styled"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Navigation = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulButcherLogo {
+        edges {
+          node {
+            butcherLogo {
+              url
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Styles>
-      <p>Here is my Navigation</p>
+      {data.allContentfulButcherLogo.edges.map(edge => {
+        return (
+          <>
+            <nav>
+              <ul className="butcher-logo">
+                <li>
+                  <img src={edge.node.butcherLogo[0].url} />
+                </li>
+              </ul>
+              <ul className="main-links">
+                <li>Food</li>
+                <li>Drink</li>
+                <li>Barrel Room</li>
+                <li>Location</li>
+              </ul>
+            </nav>
+          </>
+        )
+      })}
     </Styles>
   )
 }
--- src/components/Navigation/navigation.styled.js
+++ src/components/Navigation/navigation.styled.js
@@ -1,7 +1,22 @@
 import styled from "styled-components"
 
 const NavStyles = styled.nav`
-  color: blue;
+  nav {
+    display: flex;
+    align-items: center;
+  }
+  .butcher-logo {
+    list-style-type: none;
+    img {
+      width: 60px;
+    }
+  }
+
+  .main-links {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-around;
+  }
 `
 
 export default NavStyles
