75252d23a0be67bc0b68a6007670747e7b7c78a7
GeekLifeDevelopment <jason@geeklifedevelopment.com>
2023-05-04 11:24:15 -0500

    footer work

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,11 +1,88 @@
 import React from "react"
 import Styles from "./footer.sytled"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulButcherFooter {
+        edges {
+          node {
+            logo {
+              file {
+                url
+              }
+            }
+            header1
+            contactUs
+            header2
+            hours
+            header3
+            employment
+          }
+        }
+      }
+    }
+  `)
   return (
-    <Styles>
-      <div>Here is my footer</div>
-    </Styles>
+    <>
+      <Styles>
+        {data.allContentfulButcherFooter.edges.map(edge => {
+          return (
+            <>
+              <div className="footer-container">
+                <div className="logo-section">
+                  <img
+                    src={edge.node.logo.file.url}
+                    alt="Butcher and Barrel Logo"
+                  />
+                </div>
+                <div className="contact-section">
+                  <h3>{edge.node.header1}</h3>
+                  {edge.node.contactUs.map(contacts => {
+                    return (
+                      <>
+                        <ul>
+                          <li>{contacts}</li>
+                        </ul>
+                      </>
+                    )
+                  })}
+                </div>
+                <div className="hour-section">
+                  <h3>{edge.node.header2}</h3>
+                  {edge.node.hours.map(hour => {
+                    return (
+                      <>
+                        <ul>
+                          <li>{hour}</li>
+                        </ul>
+                      </>
+                    )
+                  })}
+                </div>
+                <div className="employment-section">
+                  <h3>{edge.node.header3}</h3>
+                  {edge.node.employment.map(employments => {
+                    return (
+                      <>
+                        <ul>
+                          <li>{employments}</li>
+                        </ul>
+                      </>
+                    )
+                  })}
+                </div>
+              </div>
+              <hr />
+              <p className="trademark">
+                © 2018 Butcher & Barrel, All Rights Reserved.
+              </p>
+            </>
+          )
+        })}
+      </Styles>
+    </>
   )
 }
 
--- src/components/Footer/footer.sytled.js
+++ src/components/Footer/footer.sytled.js
@@ -7,6 +7,46 @@ const FooterStyles = styled.footer`
   height: 100%;
   margin: 0;
   padding: 0;
+
+  .footer-container {
+    display: flex;
+    max-width: 1000px;
+    margin: 0 auto;
+    padding: 1rem 0;
+    justify-content: space-around;
+
+    ul {
+      padding: 0;
+      li {
+        list-style-type: none;
+        font-size: 14px;
+      }
+    }
+  }
+
+  .logo-section {
+    img {
+      max-width: 120px;
+    }
+  }
+
+  .contact-section {
+    li {
+    }
+  }
+
+  .hour-section {
+  }
+
+  .employment-section {
+    max-width: 300px;
+  }
+
+  .trademark {
+    text-align: center;
+    padding: 1rem 0;
+    margin: 0;
+  }
 `
 
 export default FooterStyles
